9c0d50520a864518f46a20b1cc19d5553265337e
donknap@gmail.com <donknap@gmail.com>
2023-12-07 01:08:18 +0800

    build 镜像

---
+++ app/home/http/controller/attach.go
@@ -0,0 +1,14 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/we7coreteam/w7-rangine-go/src/http/controller"
+)
+
+type Attach struct {
+	controller.Abstract
+}
+
+func (self Attach) Upload(http *gin.Context) {
+
+}
