aff8846007d98ce8c1631a00756c0b5fe2e0d870
donknap@gmail.com <donknap@gmail.com>
2024-10-31 20:23:41 +0800

    优化日显示去掉乱码

--- app/application/http/controller/run-log.go
+++ app/application/http/controller/run-log.go
@@ -1,8 +1,10 @@
 package controller
 
 import (
+	"bytes"
+	"errors"
 	"github.com/docker/docker/api/types/container"
-	"github.com/donknap/dpanel/common/function"
+	"github.com/docker/docker/pkg/stdcopy"
 	"github.com/donknap/dpanel/common/service/docker"
 	"github.com/gin-gonic/gin"
 	"github.com/we7coreteam/w7-rangine-go/v2/src/http/controller"
@@ -25,7 +27,7 @@ func (self RunLog) Run(http *gin.Context) {
 	if !self.Validate(http, &params) {
 		return
 	}
-	out, err := docker.Sdk.Client.ContainerLogs(docker.Sdk.Ctx, params.Md5, container.LogsOptions{
+	response, err := docker.Sdk.Client.ContainerLogs(docker.Sdk.Ctx, params.Md5, container.LogsOptions{
 		ShowStderr: true,
 		ShowStdout: true,
 		Tail:       strconv.Itoa(params.LineTotal),
@@ -35,18 +37,29 @@ func (self RunLog) Run(http *gin.Context) {
 		self.JsonResponseWithError(http, err, 500)
 		return
 	}
-	output, _ := io.ReadAll(out)
-	cleanOut := function.BytesCleanFunc([]rune(string(output)), func(b rune) bool {
-		return b == '\ufffd'
-	})
+	out := new(bytes.Buffer)
+	_, err = io.Copy(out, response)
+	if err != nil {
+		self.JsonResponseWithError(http, errors.New("读取日志失败"), 500)
+		return
+	}
+	newReader := bytes.NewReader(out.Bytes())
+
+	stdout := new(bytes.Buffer)
+	_, err = stdcopy.StdCopy(stdout, stdout, newReader)
+
+	if err == nil {
+		out = stdout
+	}
+
 	if params.Download {
 		http.Header("Content-Type", "text/plain")
 		http.Header("Content-Disposition", "attachment; filename="+params.Md5+".log")
-		http.Data(200, "text/plain", []byte(string(cleanOut)))
+		http.Data(200, "text/plain", []byte(out.String()))
 		return
 	} else {
 		self.JsonResponseWithoutError(http, gin.H{
-			"log": string(cleanOut),
+			"log": out.String(),
 		})
 	}
 	return
