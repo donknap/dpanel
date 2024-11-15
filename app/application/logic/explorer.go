ddbbdbc19a3553b31cdd3c9885147e8dce8fdd1c
donknap@gmail.com <donknap@gmail.com>
2024-11-15 15:33:21 +0800

    修复文件列表无法获取文件名包含空格

--- app/application/logic/explorer.go
+++ app/application/logic/explorer.go
@@ -56,7 +56,7 @@ func (self explorer) GetListByPath(path string) (fileList []*fileItemResult, err
 	//lines := strings.Split(out, "\t")
 	// 这里不能单纯的用换行进行分隔，正常的数据中会有多余的 \n
 	lines := make([][]byte, 0)
-	reg := regexp.MustCompile(`\{d>[dlb-][a-zA-Z-]{3}[a-zA-Z-]{3}[a-zA-Z-]{3}<d\}`).FindAllStringIndex(string(out), -1)
+	reg := regexp.MustCompile(`\{d>[a-zA-Z-][a-zA-Z-]{3}[a-zA-Z-]{3}[a-zA-Z-]{3}<d\}`).FindAllStringIndex(string(out), -1)
 	for i, _ := range reg {
 		line := make([]byte, 0)
 		start, end := reg[i][0], 0
