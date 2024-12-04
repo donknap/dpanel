23ee9713c68ccf67251e9eaf60cd032f81cc57c2
donknap@gmail.com <donknap@gmail.com>
2024-12-04 13:20:58 +0800

    修复加aes 加密算法错误

--- common/function/encrypt.go
+++ common/function/encrypt.go
@@ -3,40 +3,54 @@ package function
 import (
 	"bytes"
 	"crypto/aes"
+	"crypto/cipher"
 	"encoding/base64"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
-	"strings"
 )
 
 const CommonKey = "DPanelCommonAseKey20231208"
 
 func AseEncode(key string, str string) (result string, err error) {
 	key = GetMd5(CommonKey + key)
-	cipher, err := aes.NewCipher([]byte(key))
+	block, err := aes.NewCipher([]byte(key))
 	if err != nil {
 		return result, err
 	}
-	strLen := len(str)
-	for i := 0; i < 16-strLen%16; i++ {
-		str += "\n"
-	}
-	out := make([]byte, len(str))
-	cipher.Encrypt(out, []byte(str))
-	return hex.EncodeToString(out), nil
+	blockSize := block.BlockSize()
+	origStr := PKCS5Padding([]byte(str), blockSize)
+	blockMode := cipher.NewCBCEncrypter(block, []byte(key)[:blockSize])
+	crypted := make([]byte, len(origStr))
+	blockMode.CryptBlocks(crypted, origStr)
+	return hex.EncodeToString(crypted), nil
 }
 
 func AseDecode(key string, str string) (result string, err error) {
 	decodeStr, err := hex.DecodeString(str)
 	key = GetMd5(CommonKey + key)
-	cipher, err := aes.NewCipher([]byte(key))
+	block, err := aes.NewCipher([]byte(key))
 	if err != nil {
 		return result, err
 	}
-	out := make([]byte, len(decodeStr))
-	cipher.Decrypt(out, decodeStr)
-	return strings.Trim(string(out), "\n"), nil
+	blockSize := block.BlockSize()
+	blockMode := cipher.NewCBCDecrypter(block, []byte(key)[:blockSize])
+	origData := make([]byte, len(decodeStr))
+	blockMode.CryptBlocks(origData, decodeStr)
+	origData = PKCS5UnPadding(origData)
+	return string(origData), nil
+}
+
+func PKCS5Padding(plaintext []byte, blockSize int) []byte {
+	padding := blockSize - len(plaintext)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(plaintext, padtext...)
+}
+
+func PKCS5UnPadding(origData []byte) []byte {
+	length := len(origData)
+	unpadding := int(origData[length-1])
+	return origData[:(length - unpadding)]
 }
 
 func Base64Encode(obj interface{}) string {
@@ -48,10 +62,6 @@ func Base64Encode(obj interface{}) string {
 	}
 	encoder.Close()
 	return buf.String()
-}
-
-func Base64Decode(obj interface{}, enc string) error {
-	return json.NewDecoder(base64.NewDecoder(base64.StdEncoding, strings.NewReader(enc))).Decode(obj)
 }
 
 func URIEncodeComponent(s string, excluded ...[]byte) string {
