24cb88c61e819982fb2d189ac5030a6ac9c2d5bf
donknap@gmail.com <donknap@gmail.com>
2024-12-31 17:36:42 +0800

    重构 docker exec 及 docker client

---
+++ common/service/exec/option.go
@@ -0,0 +1,56 @@
+package exec
+
+import (
+	"context"
+	"os/exec"
+	"time"
+)
+
+type Option func(command *Command) error
+
+func WithArgs(args ...string) Option {
+	return func(self *Command) error {
+		if self.cmd.Path == "" {
+			self.cmd = exec.Command(args[0], args[1:]...)
+		} else {
+			self.cmd = exec.Command(self.cmd.Path, args...)
+		}
+		return nil
+	}
+}
+
+func WithCommandName(commandName string) Option {
+	return func(self *Command) error {
+		if commandName == "" {
+			return nil
+		}
+		self.cmd = exec.Command(commandName, self.cmd.Args...)
+		return nil
+	}
+}
+
+func WithTimeout(timeout time.Duration) Option {
+	return func(self *Command) error {
+		// 配置超时间后
+		ctx, _ := context.WithTimeout(context.Background(), timeout)
+		self.cmd = exec.CommandContext(ctx, self.cmd.Args[0], self.cmd.Args[1:]...)
+		return nil
+	}
+}
+
+func WithDir(dir string) Option {
+	return func(self *Command) error {
+		if dir == "" {
+			return nil
+		}
+		self.cmd.Dir = dir
+		return nil
+	}
+}
+
+func WithEnv(env []string) Option {
+	return func(self *Command) error {
+		self.cmd.Env = env
+		return nil
+	}
+}
