c6f8f68f8f29f640379bd0de2ea722a9fc13d01a
titrxw <1120309488@qq.com>
2025-03-05 15:21:38 +0800

    修改权限属性结构

--- common/accessor/permission_value_option.go
+++ common/accessor/permission_value_option.go
@@ -1,13 +1,15 @@
 package accessor
 
 type PermissionItemSetting struct {
-	Key   string      `json:"key,omitempty"`
-	Value interface{} `json:"value,omitempty"`
+	Key     string                 `json:"key,omitempty"`
+	Value   interface{}            `json:"value,omitempty"`
+	Options map[string]interface{} `json:"options,omitempty"`
 }
 
 type MenuPermission struct {
-	Permissions map[string]struct{} `json:"permissions,omitempty"`
-	Uris        map[string]struct{} `json:"uris,omitempty"`
+	Permissions map[string]struct{}    `json:"permissions,omitempty"`
+	Uris        map[string]struct{}    `json:"uris,omitempty"`
+	Options     map[string]interface{} `json:"options,omitempty"`
 }
 
 type DataPermission struct {
