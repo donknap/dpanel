62e74fbffcb76e6f591da6556bed9ff6e7d77b03
titrxw <1120309488@qq.com>
2025-05-27 18:25:44 +0800

    整理 pe ee 代码

--- common/service/family/ce.go
+++ common/service/family/ce.go
@@ -6,7 +6,6 @@ import (
 	"github.com/donknap/dpanel/common/function"
 	"github.com/donknap/dpanel/common/types"
 	"github.com/gin-gonic/gin"
-	"github.com/we7coreteam/w7-rangine-go/v2/pkg/support/console"
 	"github.com/we7coreteam/w7-rangine-go/v2/src/http/server"
 	"log/slog"
 )
@@ -14,7 +13,7 @@ import (
 type Provider struct {
 }
 
-func (self Provider) Register(httpServer *server.Server, consoleServer console.Console) {
+func (self Provider) Register(httpServer *server.Server) {
 	slog.Debug("provider load community edition")
 	httpServer.RegisterRouters(func(engine *gin.Engine) {
 		engine.POST("/api/pro/*path", notSupportedApi)
