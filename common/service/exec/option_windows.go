bb38c5939069d08b8774f3ced57c47d8355720c2
donknap@gmail.com <donknap@gmail.com>
2025-08-11 15:50:31 +0800

    兼容 windows 参数

--- common/service/exec/option_windows.go
+++ common/service/exec/option_windows.go
@@ -4,9 +4,6 @@ package exec
 
 func WithIndependentProcessGroup() Option {
 	return func(self *Command) error {
-		if self.cmd.SysProcAttr == nil {
-			self.cmd.SysProcAttr = &syscall.SysProcAttr{}
-		}
 		// windows 不支持
 		return nil
 	}
