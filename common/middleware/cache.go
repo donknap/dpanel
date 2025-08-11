82bac1a303b7dde1bc8e81f4f44b4aefab4c0c88
donknap@gmail.com <donknap@gmail.com>
2025-08-11 14:35:38 +0800

    优化静态资源 cache

--- common/middleware/cache.go
+++ common/middleware/cache.go
@@ -3,10 +3,8 @@ package common
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/we7coreteam/w7-rangine-go/v2/src/http/middleware"
-	http2 "net/http"
 	"strconv"
 	"strings"
-	"time"
 )
 
 type CacheMiddleware struct {
@@ -14,11 +12,10 @@ type CacheMiddleware struct {
 }
 
 func (self CacheMiddleware) Process(http *gin.Context) {
-	if strings.HasPrefix(http.Request.URL.Path, "/dpanel/static") {
-		defaultMaxAge := 604800
-		http.Writer.Header().Add("Cache-Control", "public, max-age="+strconv.Itoa(defaultMaxAge))
-		http.Writer.Header().Add("Expires", time.Now().Add(time.Duration(defaultMaxAge)*time.Second).UTC().Format(http2.TimeFormat))
-		http.Writer.WriteHeader(304)
+	url := http.Request.URL.Path
+	if strings.HasPrefix(url, "/dpanel/static") || strings.HasPrefix(url, "/favicon.ico") {
+		defaultMaxAge := 3600
+		http.Header("Cache-Control", "public, max-age="+strconv.Itoa(defaultMaxAge))
 	}
 	http.Next()
 	return
