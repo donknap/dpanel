700bbc270d45c264786bf35da6b56ca66a3210eb
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2025-11-19 11:56:16 +0800

    feat(docker): automatically remove stale dpanel.sock in entrypoint to prevent UDS binding failures

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,6 +2,8 @@
 
 NGINX_CONFIG_DIR="/dpanel/nginx"
 
+[ -e /var/run/dpanel/dpanel.sock ] && rm -f /var/run/dpanel/dpanel.sock
+
 chmod 755 /app/server/dpanel && mkdir -p /dpanel/nginx/default_host /dpanel/nginx/proxy_host \
   /dpanel/nginx/redirection_host /dpanel/nginx/dead_host /dpanel/nginx/temp \
   /dpanel/cert /dpanel/storage
